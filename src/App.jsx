5a6b6d5fcde1b82cc852ce74cda497876f0fbe66
moshinsimplebooks <shabeer.simplebooks@gmail.com>
2024-03-08 11:56:01 +0530

    removedDonations

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,12 @@ import Medical from './pages/Deduction/Medical/Medical'
 import Buisness from './pages/Income/Buisness/Buisness'
 import Investment from './pages/Income/Investment/Investment'
 import Housing from './pages/Deduction/Housing/Housing'
-import Donations from './pages/Deduction/Donations/Donations'
 import Educational from './pages/Deduction/Educational/Educational'
 import Other from './pages/Income/Other/Other'
 function App() {
 
   return (
     <>
-      {/* <Routes>
-        <Route path='/' element={<Main />}>
-          <Route index element={<Employee />} />
-          <Route path='/employee' element={<Employee />} />
-          <Route path='/buisness' element={<Buisness />} />
-          <Route path='/investment' element={<Investment />} />
-          <Route path='/medical' element={<Medical />} />
-          <Route path='/housing' element={<Housing />} />
-          <Route path='/educational' element={<Educational />} />
-          <Route path='/donations' element={<Donations />} />
-        </Route>
-      </Routes> */}
-
       <Routes>
         <Route path='/' element={<Main/>}>
           <Route index element={<Income/>}/>
